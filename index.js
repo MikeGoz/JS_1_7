6f7b2b74f606fb5be64eb74a2d80209f0f8e5e53
MikeGoz <gozdz@onet.pl>
2017-09-19 15:37:03 +0200

    Poprawki Task 1.7

--- index.js
+++ index.js
@@ -2,12 +2,44 @@
 
 var fs = require('fs');
 
+console.log('Input path: ');
+
+process.stdin.setEncoding('utf-8'); 
+process.stdin.on('readable', function() {
+    
+  var input = process.stdin.read();
+  var i, listOFfiles="";
+  
+  if (input !== null) {
+    var path = input.toString().trim();
+        
+    fs.readdir(path, function(err, files) {  // files- tablica
+      if (err) throw err;
+      
+      for (i = 0 ; i < files.length; i++) {
+        listOFfiles += files[i] + "\r\n";  
+      }  
+      fs.writeFile('list.txt', listOFfiles, function(err) {   
+        if (err) throw err;
+        console.log('List saved: (list.txt) :');
+        console.log(listOFfiles);
+      });
+    });
+  }
+});
+
+/*
+task 1.7 - przed poprawką
+
+var fs = require('fs');
+
 fs.readdir('./DIR', function(err, files) {
     if (err) throw err;
     console.log('Files: ',files);
-                                                               //spacja dla przejrzystości, jakby co ;)             
+                                                                           
     fs.writeFile('list.txt', files, function(err) {   
         if (err) throw err;
         console.log('Saved: list.txt');
     });
 });
+*/
